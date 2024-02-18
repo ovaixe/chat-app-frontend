5a8d6ecdd984aa6abc0e0031e78130461b415b28
Bhat Owais <owaisbhat996@outlook.com>
2024-02-19 02:12:14 +0530

    Updated chat room UI

--- src/components/room/chatbox/Message.tsx
+++ src/components/room/chatbox/Message.tsx
@@ -7,7 +7,7 @@ export default function Message(props: {
   timeSent = new Date(timeSent);
 
   return (
-    <div className="flex flex-col p-3 space-y-2 bg-slate-800 rounded-2xl max-w-fit">
+    <div className="flex flex-col p-2 space-y-2 bg-slate-800 rounded-xl max-w-fit">
       <div className="flex flex-row space-x-3 justify-between">
         <div className="flex flex-row justify-center items-center space-x-2">
           <div className="w-5 h-5 rounded-full bg-green-300 text-stone-800 text-center text-bold text-sm">
@@ -16,7 +16,11 @@ export default function Message(props: {
           <div className="text-sm text-green-500">{userName}</div>
         </div>
         <div className="flex justify-end text-stone-400 text-sm">
-          {timeSent.getHours()}:{timeSent.getMinutes()}
+          {timeSent.toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+          })}
         </div>
       </div>
       <div className="text-sm text-white text-bold">{message}</div>
