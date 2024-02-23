7288ab2c6204f86b69be27c376ca431877f678c2
Bhat Owais <owaisbhat996@outlook.com>
2024-02-23 05:45:10 +0530

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        popOut: {
-          "0%": { transform: "scale(0)" },
-          "100%": { transform: "scale(1)" },
-        },
-      },
-      animation: {
-        popOut: "popOut 0.3s ease-in-out",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        pattern: "url('/img/chat.jpg')",
+export const content = [
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    keyframes: {
+      popOut: {
+        "0%": { transform: "scale(0)" },
+        "100%": { transform: "scale(1)" },
       },
     },
+    animation: {
+      popOut: "popOut 0.3s ease-in-out",
+    },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      pattern: "url('/img/chat.jpg')",
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
